523569413d3d30dad25b0fca9f0cbbe8f577e524
pullipulli <pulli@Nitro5_Pulli.localdomain>
2023-10-04 15:17:55 +0200

    Profile concept (just text)

--- PWM_SNM_react/src/pages/Profile.jsx
+++ PWM_SNM_react/src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import {Button} from "@mui/material";
+import {Button, Stack, Typography} from "@mui/material";
 import {useNavigate} from "react-router-dom";
 import routes from "../utils/routes.jsx";
 import {useEffect} from "react";
@@ -21,7 +21,28 @@ export default function Profile() {
     }
 
     return <>
-        <div>Profile of {getUser()?.username}</div>
+        <Stack>
+            <Typography variant="caption">Nome utente: {getUser()?.username}</Typography>
+            <Typography variant="caption">Nome: {getUser()?.name}</Typography>
+            <Typography variant="caption">Cognome: {getUser()?.surname}</Typography>
+            <Typography variant="caption">Email: {getUser()?.email}</Typography>
+            <Typography variant="caption">Generi preferiti: </Typography>
+            <Stack>
+                {getUser()?.favouriteGenres?.map((genre) => {
+                    return <Typography key={genre}
+                                       variant="caption">{genre._id}</Typography>;
+                })}
+
+            </Stack>
+            <Typography variant="caption">Artisti preferiti: </Typography>
+            <Stack>
+                {getUser()?.favouriteArtists?.map((artist) => {
+                    return <Typography key={artist}
+                                       variant="caption">{artist.artist.name}</Typography>;
+                })}
+            </Stack>
+        </Stack>
+        <Button>Voglio modificare i miei dati</Button> {/*TODO*/}
         <Button onClick={handleLogout}>Logout</Button>
     </>
 }
